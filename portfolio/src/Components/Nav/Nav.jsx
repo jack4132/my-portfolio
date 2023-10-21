b8252c783ededcf2d80510e074e7f3a2e6b71eaf
Jackson Thounaojam <jacksonthounaojam1@gmail.com>
2023-10-21 15:05:27 +1100

    fix minor issue

--- portfolio/src/Components/Nav/Nav.jsx
+++ portfolio/src/Components/Nav/Nav.jsx
@@ -4,7 +4,7 @@
  * File Created: Friday, 20th October 2023 3:18:06 pm
  * Author: Jackson Thounaojam (jackson.thounaojam@mutualmobile.com)
  * -----
- * Last Modified: Saturday, 21st October 2023 12:14:50 am
+ * Last Modified: Saturday, 21st October 2023 3:04:42 pm
  * Modified By: Jackson Thounaojam (jackson.thounaojam@mutualmobile.com)
  * -----
  * Copyright 2020 - 2023 Mutual Mobile, Mutual Mobile
@@ -12,13 +12,16 @@
 import React from 'react';
 import {  Link } from "react-router-dom";
 import './Nav.css'
+import {useLocation} from "react-router-dom"
 
 const Nav = () => {
+    const location=useLocation()
+    console.log(location)
     return (
         <div className='background-primary' style={{width:'100%',position:'sticky',top:0,right:0,zIndex:10}}>
             <div className='nav-container'>
                 <div ><Link to="/" className='logo-name'>Jackson Thounaojam</Link></div> 
-             <div className='nav-menu'><Link className="nav-txt" to="/">Home</Link><Link to="/about" className="nav-txt">About</Link></div>
+             <div className='nav-menu'><Link className={`nav-txt ${location?.pathname ==="/" && "underline-txt"}`} to="/">Home</Link><Link to="/about" className={`nav-txt ${location?.pathname ==="/about" && "underline-txt"}`}>About</Link></div>
             </div>
         </div>
     );
